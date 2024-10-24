445cb4593f2f878c996e8b910f41013ecfe25e5d
hhejo <litsmainjof@gmail.com>
2024-10-24 14:43:57 +0900

    feat: update create item

--- src/index.js
+++ src/index.js
@@ -30,30 +30,3 @@ document.addEventListener('item-deleted', async (e) => {
   printItems(items);
   setTotalPrice(items);
 });
-
-async function createItemHandler(e) {
-  e.preventDefault();
-  let item = (() => {
-    let $selectedOption = document.querySelector(
-      'input[name="option"]:checked'
-    );
-    let checkedType = $selectedOption
-      ? $selectedOption.nextElementSibling.textContent.trim()
-      : null;
-    let date = document.getElementById('date').value.trim(); // 날짜
-    let price = +document.getElementById('price').value.trim().replace(',', ''); // 가격
-    let content = document.getElementById('content').value.trim(); // 내용
-    price = checkedType === '지출' ? -price : price;
-    return { date, price, content };
-  })(); // 아이템
-  if (!item.date || !item.content) return;
-  let itemAddEvent = new CustomEvent('item-added', { detail: { item } });
-  document.dispatchEvent(itemAddEvent);
-  hideCreateItemFormHandler();
-}
-window.createItemHandler = createItemHandler;
-
-function createItemSubmitHandler(e) {
-  e.preventDefault();
-}
-window.createItemSubmitHandler = createItemSubmitHandler;
